737bfeb254809106b06dbad391fec5005e87a134
Pierre Mevel <pierre.mevel@student.ecp.fr>
2019-01-08 17:47:44 +0100

    port definition

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const nodemailer = require("nodemailer");
 // Only needed if you don't"nodemailer" mail account for testing
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
