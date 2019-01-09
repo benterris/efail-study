23361b4650cd4029f3d6b5d1ff459ab6acbcea39
Pierre Mevel <pierre.mevel@student.ecp.fr>
2019-01-09 17:50:57 +0100

    decode URL

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/", (req, res, next) => {
-  console.log(req.originalUrl);
+  console.log(decodeURI(req.originalUrl)); // eslint-disable-line
   next();
 });
 
